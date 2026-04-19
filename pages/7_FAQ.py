14dcfeb137c2e177ebe55963e7a2c319d94c62af
Vu Ngoc Huong Tran <92384719+huongtran182@users.noreply.github.com>
2026-04-18 22:56:00 -0500

    Update last update date in FAQ footer

--- pages/7_FAQ.py
+++ pages/7_FAQ.py
@@ -249,7 +249,7 @@ render_faq_section("TOOLS", tool_faqs)
 # footers
 st.markdown("""
 <div style="text-align: center; font-size:14px; color:#2a2541;">
-  <em>Last update: Jul 2025</em>
+  <em>Last update: Apr 2026</em>
 </div>
 """, unsafe_allow_html=True)
 

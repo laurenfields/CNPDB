1692902ab2b012ccc7e2ab6386cd37b7d501007e
Vu Ngoc Huong Tran <92384719+huongtran182@users.noreply.github.com>
2026-04-18 22:55:19 -0500

    Update last update date to April 2026

--- pages/5_Statistics.py
+++ pages/5_Statistics.py
@@ -136,7 +136,7 @@ else:
 
 st.markdown("""
 <div style="text-align: center; font-size:14px; color:#2a2541;">
-  <em>Last update: Jul 2025</em>
+  <em>Last update: Apr 2026</em>
 </div>
 """, unsafe_allow_html=True)
 

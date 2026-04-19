e44651e84a3dc5b3b78f1f97284db42c4b6c975d
Vu Ngoc Huong Tran <92384719+huongtran182@users.noreply.github.com>
2026-04-18 22:59:47 -0500

    Update last update date to April 2026

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,6 +125,6 @@ Copyright 2025 Department of Pharmaceutical Sciences, University of Wisconsin-Ma
 
 st.markdown("""
 <div style="text-align: center; font-size: 14px; color: #2a2541;">
-  <em>Last update: Jul 2025</em>
+  <em>Last update: Apr 2026</em>
 </div>
 """, unsafe_allow_html=True)

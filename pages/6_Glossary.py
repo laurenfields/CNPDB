4f92681815ff1abd85b0368edc600931d77fc3a7
Vu Ngoc Huong Tran <92384719+huongtran182@users.noreply.github.com>
2026-04-18 22:55:41 -0500

    Update last update date in glossary

--- pages/6_Glossary.py
+++ pages/6_Glossary.py
@@ -182,7 +182,7 @@ render_section("Species", species)
 
 st.markdown("""
 <div style="text-align: center; font-size:14px; color:#2a2541;">
-  <em>Last update: Jul 2025</em>
+  <em>Last update: Apr 2026</em>
 </div>
 """, unsafe_allow_html=True)
 

c4cd1736cb6cf981dc8eca17c8f8116331a59c7b
Vu Ngoc Huong Tran <92384719+huongtran182@users.noreply.github.com>
2026-07-12 10:05:39 -0500

    Update 3_Related_Databases.py

--- pages/3_Related_Databases.py
+++ pages/3_Related_Databases.py
@@ -375,7 +375,7 @@ for row_index in range(num_rows):
 
 st.markdown("""
 <div style="text-align: center; font-size:14px; color:#2a2541;">
-  <em>Last update: Apr 2026</em>
+  <em>Last update: Jul 2026</em>
 </div>
 """, unsafe_allow_html=True)
 
